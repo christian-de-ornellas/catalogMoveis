534f9e2713432c6741ee4291b9b50596d61aefce
Christian Possidonio <christian@orientme.com.br>
2019-04-07 15:26:54 -0300

    Incluindo instacia da Classe User, Movie e Cast no shell para que possamos gerar o arquivo do banco sqlite via shell do python

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from app import app, db
+from app.models import User, Movie, Cast
 
 # Este decorate retorna as features no terminal
 @app.shell_context_processor
 def shell_conext():
     return dict(
         app=app,
-        db=db
+        db=db,
+        User=User,
+        Movie=Movie,
+        Cast=Cast
     )
 if __name__ == "__main__":
     app.run(debug=True)
