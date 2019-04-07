8513ab31c7930905972b56cf4bcef3fb89dc5602
Christian Possidonio <christian@orientme.com.br>
2019-04-07 15:13:39 -0300

    conexao com o sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+# Caminho absoluto para o banco
+basedir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "storage.db")
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:////{basedir}"
 api = Api(app)
 db = SQLAlchemy(app)
 CORS(app)
