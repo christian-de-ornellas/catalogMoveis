1255fc04f28f01b7d530af11f39c9d952879c0aa
Christian Possidonio <christian@orientme.com.br>
2019-04-07 16:50:39 -0300

    Importaçao das rotas auth

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from app import app, db
 from app.models import User, Movie, Cast
 
 # Este decorate retorna as features no terminal
+# Por exemplo, no terminal conseguimos gerar o 
+# arquivo storage.db, persistir no banco via terminal
+# entre outros recursos.
 @app.shell_context_processor
 def shell_conext():
     return dict(
