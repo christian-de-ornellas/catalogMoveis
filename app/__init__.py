5c77a0316941da697db18b965bef9223ffa48251
Christian Possidonio <christian@orientme.com.br>
2019-04-07 14:38:24 -0300

    Arquivo de inicializaçao da aplicaçao

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+api = Api(app)
+db = SQLAlchemy(app)
+CORS(app)
+
